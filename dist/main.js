87bf8e48a4517543273f56c79582aa94f924eb50
pxMahio <ron.wollschlaeger@frontshape.de>
2023-09-30 16:31:06 +0300

    Upload

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,4 @@
+//Template JavaScript
 // Dark Mode Toggle
 // Icons
 const sunIcon = document.querySelector(".sun");
@@ -46,3 +47,54 @@ moonIcon.addEventListener("click", () => {
 
 // invoke theme check on initial load
 themeCheck();
+
+
+//Project JavaScript
+//btn-follow
+function toggleFollow() {
+    var button = document.getElementById("btn-follow");
+    if (button.innerHTML === "Follow") {
+        button.classList.remove("border-gray-800");
+        button.classList.remove("bg-gray-800");
+        button.classList.remove("hover:border-gray-500");
+        button.classList.remove("hover:bg-gray-500");
+        button.classList.add("border-gray-200");
+        button.classList.add("text-gray-200");
+        button.innerHTML = "Following";
+    } else {
+        button.classList.add("border-gray-800");
+        button.classList.add("bg-gray-800");
+        button.classList.add("hover:border-gray-500");
+        button.classList.add("hover:bg-gray-500");
+        button.classList.remove("border-gray-200");
+        button.classList.remove("text-gray-200");
+        button.innerHTML = "Follow";
+    }
+}
+
+function toggleFollow2() {
+    var button = document.getElementById("btn-modular_follow");
+    if (button.innerHTML === "Follow") {
+        button.innerHTML = "Following";
+    } else {
+        button.innerHTML = "Follow";
+    }
+}
+
+// btn-download
+function toggleDownload() {
+    alert("No download here...  (ノ ゜Д゜)ノ ︵ ┻━┻");
+}
+
+//btn-share
+function toggleShare() {
+    alert("Nothing to share here... (╥﹏╥)")
+}
+
+//modular
+
+
+//btn-modular_close
+function toggleModularCole() {
+
+}
